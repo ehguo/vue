f5fb90edbfe88318d18908392731385548f31162
Edward Guo <edward.h.guo@gmail.com>
2017-10-01 22:00:40 -0700

    Add a few examples

--- src/vue.js
+++ src/vue.js
@@ -1,6 +1,20 @@
-new Vue({
-  el: '#root',
+const hello = new Vue({
+  el: '#hello',
   data: {
     hello: 'Hello World!'
+  }
+})
+
+const title = new Vue({
+  el: '#title',
+  data: {
+    title: new Date()
+  }
+})
+
+const conditional = new Vue({
+  el: '#conditional',
+  data: {
+    conditional: true
   }
 })
